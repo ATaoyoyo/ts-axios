e37e07314fd7a578d2ab31f11bc9e1698454e2e4
ATaoyoyo <ataoyoyo@ataoyoyo.local>
2020-12-26 11:18:06 +0800

    feat: 处理请求body数据

--- src/helps/utils.ts
+++ src/helps/utils.ts
@@ -1,9 +1,13 @@
 const toString = Object.prototype.toString
 
 export function isDate(val: any): boolean {
-  return toString.call(val) === '[Object Date]'
+  return toString.call(val) === '[object Date]'
 }
 
-export function isObject(val: any): boolean {
-  return val !== null && typeof val === 'object'
+// export function isObject(val: any): boolean {
+//   return val !== null && typeof val === 'object'
+// }
+
+export function isPlainObject(val: any): boolean {
+  return toString.call(val) === '[object Object]'
 }
